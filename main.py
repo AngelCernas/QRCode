ba1f96f95e710bb3963348920712025cc43e923a
Angel Cernas <angel_210899@hotmail.com>
2024-11-27 11:49:31 -0600

    creacio excel

--- main.py
+++ main.py
@@ -24,17 +24,45 @@ while(capture.isOpened()):
         tarima = fraccionada[1]
         area = fraccionada[2]
         bloque = fraccionada[3]
+        Px = fraccionada[4]
+        Py = fraccionada[5]
+        Pz = fraccionada[6]
+        bultos = fraccionada[7]
+        peso = fraccionada[8]
+        cubicaje = fraccionada[9]
+        marca = fraccionada[10]
+        embalaje = fraccionada[11]
+
         print(consecutivo)
         print(tarima)
         print(area)
         print(bloque)
+        print(Px)
+        print(Py)
+        print(Pz)
+        print(bultos)
+        print(peso)
+        print(cubicaje)
+        print(marca)
+        print(embalaje)
+
+
         time.sleep(3)
 
         parametros = {
             'Consecutivo': [f'{consecutivo}'],
             'Tarima': [f'{tarima}'],
             'Area': [f'{area}'],
-            'Bloque': [f'{bloque}']
+            'Bloque': [f'{bloque}'],
+            'Posicion X': [f'{Px}'],
+            'Posicion y': [f'{Py}'],
+            'Posicion Z': [f'{Pz}'],
+            'Bultos': [f'{bultos}'],
+            'Peso': [f'{peso}'],
+            'Cubicaje': [f'{cubicaje}'],
+            'Marca': [f'{marca}'],
+            'Embalaje': [f'{embalaje}']
+
         }
         df = pd.DataFrame(parametros)
         df.to_excel('inventario.xlsx', index=False)  # index=False para no incluir los índices en el archivo
